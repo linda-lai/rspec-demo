413baa2bbf6c6584d0599a1945e8ed7daea16505
Linda Lai <lindalai.615@gmail.com>
2018-12-08 23:02:46 +1100

    Create basic initial fail/pass test cases

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,1 +1,7 @@
+class Calculator
 
+    def calculate_odd_or_even(test_num)
+        test_num % 2 == 0 ? true : false
+    end
+    
+end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,2 +1,21 @@
 require 'calculator'
 require 'spec_helper'
+
+RSpec.describe Calculator do
+    context "when initialised" do
+        it "should be an instance of the class Calculator" do
+            expect(subject).to be_a Calculator
+        end
+    end
+
+    context "when passed a number" do
+        it "should return true if even" do
+            expect(subject.calculate_odd_or_even(2)).to eq true
+        end
+
+        it "should return false if odd" do
+            expect(subject.calculate_odd_or_even(3)).to eq false
+        end
+    end
+
+end
